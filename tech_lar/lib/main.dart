3d2428d758c647cd4156c7d736dc2036c2da9c50
HappySalamander <lopesgustavo0807@gmail.com>
2024-06-18 11:56:21 -0300

    Update main.dart

--- tech_lar/lib/main.dart
+++ tech_lar/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'home.dart';
+import 'login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home Page'),
+      home:  LoginPage(),
     );
   }
 }
